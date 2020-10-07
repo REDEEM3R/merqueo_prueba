2b8a498dad4198c4ec7005b6ec08b30c9a85394a
Camilo Ovalles <camilo@2ovalles.com>
2020-10-07 15:45:52 -0500

    #000 comienza el proyecto - 7 octubre 2020

---
+++ prueba_tecnica_merqueo_kotlin/app/src/main/java/com/example/prueba_tecnica_merqueo_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prueba_tecnica_merqueo_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
