e6ca71e6ccbb35d59b31ed0033d5e91ac0513b9c
Camilo Ovalles <camilo@2ovalles.com>
2020-10-09 13:07:55 -0500

    git commit -m "9 de octubre 2020 recyclerview inicio de implementacion, adaptador, actividad  y viewholder"

--- prueba_tecnica_merqueo_kotlin/app/src/main/java/com/example/prueba_tecnica_merqueo_kotlin/MainActivity.kt
+++ prueba_tecnica_merqueo_kotlin/app/src/main/java/com/example/prueba_tecnica_merqueo_kotlin/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.prueba_tecnica_merqueo_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+
+
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
     }
 }
